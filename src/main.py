3abb902ab7f848b28b586163b1b70ff51af8639e
marlecce <lecce_marco@yahoo.it>
2013-08-01 10:38:31 +0200

    test wx import

---
+++ src/main.py
@@ -0,0 +1,17 @@
+'''
+Created on Aug 1, 2013
+
+@author: mlecce
+'''
+import wx
+
+class MyApp(wx.App):
+    def __init__(self, redirect=False, filename=None):
+        print("creating window...")
+        wx.App.__init__(self, redirect, filename)
+        self.frame = wx.Frame(None, wx.ID_ANY, title='My Title')
+        self.panel = wx.Panel(self.frame, wx.ID_ANY)
+
+if __name__ == '__main__':
+    app = MyApp()
+    app.MainLoop()
