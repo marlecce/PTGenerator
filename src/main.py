9bb3a9116d2490bb6a4613b127becaa3f9dfceac
marlecce <lecce_marco@yahoo.it>
2013-08-01 10:45:51 +0200

    test main windows created

--- src/main.py
+++ src/main.py
@@ -5,13 +5,27 @@ Created on Aug 1, 2013
 '''
 import wx
 
-class MyApp(wx.App):
-    def __init__(self, redirect=False, filename=None):
-        print("creating window...")
-        wx.App.__init__(self, redirect, filename)
-        self.frame = wx.Frame(None, wx.ID_ANY, title='My Title')
-        self.panel = wx.Panel(self.frame, wx.ID_ANY)
+class MyApp(wx.Frame):
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title=title, size=(400,300))
+        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.CreateStatusBar() # A Statusbar in the bottom of the window
+
+        # Setting up the menu.
+        filemenu= wx.Menu()
+
+        # wx.ID_ABOUT and wx.ID_EXIT are standard IDs provided by wxWidgets.
+        filemenu.Append(wx.ID_ABOUT, "&About"," Information about this program")
+        filemenu.AppendSeparator()
+        filemenu.Append(wx.ID_EXIT,"E&xit"," Terminate the program")
+
+        # Creating the menubar.
+        menuBar = wx.MenuBar()
+        menuBar.Append(filemenu,"&File") # Adding the "filemenu" to the MenuBar
+        self.SetMenuBar(menuBar)  # Adding the MenuBar to the Frame content.
+        self.Show(True)
 
 if __name__ == '__main__':
-    app = MyApp()
+    app = wx.App(False)
+    frame = MyApp(None, "Main Window")
     app.MainLoop()
