5563003fd042cc03caa7c0b2b711509a0d142dca
marlecce <lecce_marco@yahoo.it>
2013-08-07 17:00:57 +0200

    bug fixed in bitly short url

--- src/bitly.py
+++ src/bitly.py
@@ -8,14 +8,9 @@ source: http://segfault.in/2010/10/shorten-urls-using-python-and-bit-ly/
 # Import the modules
  
 import bitly_api
-import sys
  
 def shorten(url):
     API_USER = ""
     API_KEY = ""
     b = bitly_api.BitLy(API_USER, API_KEY)
-    # Define how to use the program
-    if len(sys.argv) != 2:
-        sys.exit(0)
-    longurl = sys.argv[1]
-    return b.shorten(longUrl=longurl)
+    return b.shorten(url)
